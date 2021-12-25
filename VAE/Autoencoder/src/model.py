ceb61de597987f72b77937a11021dd3fbb8beb17
lim-yong-jae <62493307+lim-yong-jae@users.noreply.github.com>
2021-12-25 19:45:11 +0900

    Update model.py

--- VAE/Autoencoder/src/model.py
+++ VAE/Autoencoder/src/model.py
@@ -1,1 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class VAE(nn.Module):
+    def __init__(self, x_dim, h_dim1, h_dim2, z_dim):
+        super(VAE, self).__init__()
+
+        # encoder part
+        self.fc1 = nn.Linear(x_dim, h_dim1)
+        self.fc2 = nn.Linear(h_dim1, h_dim2)
+        self.fc31 = nn.Linear(h_dim2, z_dim)
+        self.fc32 = nn.Linear(h_dim2, z_dim)
+        # decoder part
+        self.fc4 = nn.Linear(z_dim, h_dim2)
+        self.fc5 = nn.Linear(h_dim2, h_dim1)
+        self.fc6 = nn.Linear(h_dim1, x_dim)
+
+    def encoder(self, x):
+        h = F.relu(self.fc1(x))
+        h = F.relu(self.fc2(h))
+        return self.fc31(h), self.fc32(h)  # mu, log_var
+
+    def sampling(self, L, mu, log_var):
+        z = []
+        for i in range(0, L):
+            std = torch.exp(0.5 * log_var)
+            z.append(mu + torch.randn_like(std))
+
+        z = torch.stack(z, dim = 0);
+        return z  # return z sample
+
+    def decoder(self, z):
+        h = F.relu(self.fc4(z))
+        h = F.relu(self.fc5(h))
+        return F.sigmoid(self.fc6(h))
+
+    def forward(self, L, x):
+        mu, log_var = self.encoder(x.view(-1, 784))
+        z = self.sampling(L, mu, log_var)
+        return self.decoder(z), mu, log_var
